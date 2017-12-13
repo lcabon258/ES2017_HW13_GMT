110cc1b37e88c9d6bb3d1ef0b2faf527dc723902
lcabon258 <b01204033@ntu.edu.tw>
2017-12-13 22:40:24 +0800

    修正dos指令與沒有預設-R-J的問題

--- PlateMotionHomework.bat
+++ PlateMotionHomework.bat
@@ -1,22 +1,22 @@
 @ECHO OFF
 set ps=PlateMotionTemp.ps
-set save=PlateMotion1.ps &REM 設定存檔的檔名
-set InputVector=Vector.txt &REM 設定讀入的向量檔
-set StudentID=r05224123 &REM 設定自己的學號
-set Model="NUVEL 1A" &REM 設定模型的名稱
+set save=PlateMotion1.ps
+set InputVector=Vector.txt
+set StudentID=r05224123
+set Model="NUVEL 1A"
 
 REM ===== ===== =====
 echo "Plot Vectors"
 REM Velocity Legend
-copy %ps% %save%
-echo 140 2 40 0 0 0 0 Legend | gmt psvelo -R -J -O -K -Se0.5p/0.95/0p -W1p,black,solid -A+e+gblack -P >> %save%
+copy /A %ps% %save%
+echo 140 2 40 0 0 0 0 Legend | gmt psvelo -R118/150/0/41.6 -JM15c -O -K -Se0.5p/0.95/0p -W1p,black,solid -A+e+gblack -P >> %save%
 echo 143.5 2 40 mm/yr | gmt pstext -R -J -O -K -F+f12p,Helvetica,black+jLM >> %save%
 
 REM Model Name 
-echo 140 1 Model \: %Model% | gmt pstext -R -J -O -K -F+f12p,Helvetica,black+jLM  << EOF >> %save%
+echo 140 1 Model \: %Model% | gmt pstext -R -J -O -K -F+f12p,Helvetica,black+jLM  >> %save%
 
 REM Velocity 
-gmt psvelo %InputVector% -R -J -Se0.5p/0.95/0p -W1p,black,solid -A+e+gblack -O -K -P -h1>> %save%
+gmt psvelo %InputVector% -R -J -Se0.5p/0.95/0p -W1p,black,solid -A+e+gblack -O -K -P -h1 >> %save%
 REM 
 
 echo "Plot Author"
@@ -24,3 +24,4 @@ echo 140 6 Student ID : %StudentID% | gmt pstext -R -J -O -K -F+f12p,Helvetica,b
 
 gmt psxy -R -J -O -T -UBL/0/0/"%StudentID%@@ntu.edu.tw" >> %save%
 gmt psconvert -Tj -E300 -A1c %save%
+pause
